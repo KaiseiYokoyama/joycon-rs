2a4c6d542e553e7f85b297edebea643b8be125ef
Kaisei Yokoyama <yokoyama.kaisei.sm@alumni.tsukuba.ac.jp>
2020-05-28 18:39:42 +0900

    #27 SPIの記録を取得

--- examples/spi.rs
+++ examples/spi.rs
@@ -58,19 +58,76 @@ fn main() -> JoyConResult<()> {
                 Ok(device) => device,
                 Err(e) => e.into_inner(),
             };
-            device.send_feature_report(&[0x71,0x3D,0x60,0x0,0xF8,0x12,0x0,0xE8])?;
-            dbg!(line!());
+
+            //[
+            //     61,
+            //     96,
+            //     0,
+            //     0,
+            //     8,
+            // ]
+            // dbg!(&spi_target(0x603D, 9));
+            device.write(&[0x01, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x10, 0x3D, 0x60, 0, 0, 18])?;
             {
                 let mut buf = [0u8;64];
-                device.get_feature_report(&mut buf)?;
-                dbg!([buf[0],buf[1],buf[2],buf[3],buf[4]]);
+                // device.get_feature_report(&mut buf)?;
+                device.read(&mut buf)?;
+                // let mut reply = [0u8;18];
+                // reply.copy_from_slice(&buf[15..32]);
+                // println!("Input report: {:?}", buf[0]);
+                // println!("Factory: {:?}", buf[13..32].to_vec());
+                let mut report = [0u8;18];
+                report.copy_from_slice(&buf[20..38]);
+                let cab = AnalogStickCalibrations::from(report);
+                dbg!(cab);
             }
+            // device.write(&[0x01, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x10, 0x00, 134, 0x60, 0, 0, 18])?;
+            // {
+            //     let mut buf = [0u8;64];
+            //     device.get_feature_report(&mut buf)?;
+                // device.read(&mut buf)?;
+                // println!("Input report: {:?}", buf[0]);
+                // println!("Factory: {:?}", buf[13..32].to_vec());
+                // AnalogStickCalibrations::from()
+            // }
+            // device.send_feature_report(&[0x71,0x3D,0x60,0x0,0xF8,0x12,0x0,0xE8])?;
+            // {
+            //     let mut buf = [0u8;64];
+            //     device.get_feature_report(&mut buf)?;
+            //     device.read(&mut buf)?;
+            //     dbg!([buf[13..32].to_vec()]);
+            // }
+            // {
+            //     let mut buf = [0u8;64];
+            //     device.get_feature_report(&mut buf)?;
+                // device.read(&mut buf)?;
+                // dbg!([buf[13..32].to_vec()]);
+            // }
+            // {
+            //     let mut buf = [0u8;64];
+            //     device.get_feature_report(&mut buf)?;
+                // device.read(&mut buf)?;
+                // dbg!([buf[13..32].to_vec()]);
+            // }
+            // {
+            //     let mut buf = [0u8;64];
+            //     // device.get_feature_report(&mut buf)?;
+            //     device.read(&mut buf)?;
+            //     dbg!([buf[13..32].to_vec()]);
+            // }
+            // {
+            //     let mut buf = [0u8;64];
+            //     // device.get_feature_report(&mut buf)?;
+            //     device.read(&mut buf)?;
+            //     dbg!([buf[13..32].to_vec()]);
+            // }
 
             let mut driver = SimpleJoyConDriver::new(&d)?;
+            dbg!(FactoryStickCalibrations::once(&mut driver));
             dbg!(line!());
 
-            let cal = FactoryStickCalibrations::once(&mut driver)?;
-            dbg!(cal);
+            // let cal = FactoryStickCalibrations::once(&mut driver)?;
+            // dbg!(cal);
 
             Ok(())
         })?;
