398f01516f08fb52d4cbbdca42e7ab905d43672f
haruna <w10776e8w@yahoo.co.jp>
2021-12-11 06:39:06 +0900

    rm: useless test

--- src/lib.rs
+++ src/lib.rs
@@ -263,11 +263,3 @@ pub mod result {
 
     pub type JoyConResult<T> = Result<T, JoyConError>;
 }
-
-// #[cfg(test)]
-// mod tests {
-//     #[test]
-//     fn it_works() {
-//         assert_eq!(2 + 2, 4);
-//     }
-// }
